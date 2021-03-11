3a130965112249f3f41f9ae15109c9a84e61d44e
Benaya Trabelsi <Benaya7@gmail.com>
2021-03-11 15:25:36 +0200

    .

--- functionalToDoForTesting/toDo.test.js
+++ functionalToDoForTesting/toDo.test.js
@@ -9,9 +9,9 @@ import {
   list,
 } from './toDo.js';
 console.log(testAddItem(toDo));
-// test('just see!', () => {
-//   expect(testAddItem(toDo)).toBe('passed add item');
-//   expect(testDeleteItem(toDo)).toEqual([]);
-//   expect(testAddItem(toDo)).toBe('passed add item');
-//   expect(testMarkAsDone(toDo)).toEqual([true, true, true]);
-// });
+test('just see!', () => {
+  expect(testAddItem(toDo)).toBe('passed add item');
+  expect(testDeleteItem(toDo)).toEqual([]);
+  expect(testAddItem(toDo)).toBe('passed add item');
+  expect(testMarkAsDone(toDo)).toEqual([true, true, true]);
+});
